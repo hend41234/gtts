daced30b4a48aa3c6213e653993b02425d7b1e2e
endbot <hendri41234@gmail.com>
2025-06-08 20:52:36 +0700

    update name Key of env file

--- utilstts/utils.go
+++ utilstts/utils.go
@@ -13,14 +13,14 @@ type EndpointsModel struct {
 }
 
 type UtilizationModel struct {
-	API_KEY string `json:"API_KEY"`
+	API_KEY string `json:"GC_API_KEY"`
 	BaseURL string `json:"BASE_URL"`
 	// Endpoint []EndpointsModel `json:"ENDPOINT"`
 }
 
 var Utils *UtilizationModel
 
-// require to use, you must have the API KEY of gemini, the key is API_KEY in the env file
+// require to use, you must have the API KEY of gemini, the key is GC_API_KEY in the env file
 //
 // if you do not have the API KEY, you can open the url : https://console.cloud.google.com/apis/credentials
 func LoadEnv(envFile string) {
@@ -30,14 +30,28 @@ func LoadEnv(envFile string) {
 		if envErr != nil {
 			log.Fatal("API_KEY not found, or env file nor found")
 			// time.Sleep(2 * time.Second)
-			// continue
+			continue
 		}
 		break
 	}
 	godotenv.Load(".env")
 
-	Utils.API_KEY = os.Getenv("API_KEY")
+	Utils.API_KEY = os.Getenv("GC_API_KEY")
 	// Utils.BaseURL = os.Getenv("BASE_URL")
 	Utils.BaseURL = "https://texttospeech.googleapis.com"
+}
+
+func init() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		return
+	}
+	Utils = new(UtilizationModel)
+	gcApiKey := os.Getenv("GC_API_KEY")
+	if gcApiKey != "" {
+		Utils.API_KEY = gcApiKey
+	}
+	Utils.BaseURL = "https://texttospeech.googleapis.com"
+	return
 
 }
